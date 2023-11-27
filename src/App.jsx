72167f8848a9cb953f5ebffa15717d7702ebcd1d
Jpark <web.jpark@gmail.com>
2023-11-27 15:07:12 -0700

    Establish routes and develop foundational pages for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './layouts/Home';
+import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder from './features/order/CreateOrder';
+import Order from './features/order/Order';
+import Root from './layouts/Root';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Root />,
+		children: [
+			{ path: '/', element: <Home /> },
+			{ path: '/menu', element: <Menu /> },
+			{ path: '/cart', element: <Cart /> },
+			{ path: '/order/new', element: <CreateOrder /> },
+			{ path: '/order/:orderId', element: <Order /> },
+		],
+	},
+]);
+
 export default function App() {
-	return <div>Hello!</div>;
+	return <RouterProvider router={router} />;
 }

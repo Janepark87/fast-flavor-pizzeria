6643621ff47b21651ed6f70370cf131b2cae81d9
Jpark <web.jpark@gmail.com>
2023-12-12 14:07:11 -0700

    fixed api url

--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.DEV ? '/api' : `/fast-flavor-pizzeria/${import.meta.PIZZA_MENU_API_URL}/api`;
+const API_URL = import.meta.env.DEV ? '/api' : `${import.meta.env.PIZZA_MENU_API_URL}/api`;
 
 export async function getMenu() {
 	const res = await fetch(`${API_URL}/menu`);

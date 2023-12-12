cbd55fabb0ddf350931984b618fec91fab2aa8ab
Jpark <web.jpark@gmail.com>
2023-12-12 13:18:08 -0700

    remove commented-out code

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-// 'https://react-fast-pizza-api.onrender.com'
 export default defineConfig(({ command, mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
 	const config = {
